52025132cbcfda24697c93f3004d756121c66a3e
belenvivas <mbelenvivas@gmail.com>
2023-01-31 19:57:49 -0300

    agregado el trabajo Integrador final

---
+++ public/js/script.js
@@ -0,0 +1,78 @@
+function imprimirDatosComplejos(seccion, arreglo) {
+    var elementos = '';
+    for (var i = 0; i < arreglo.length; i++) {
+        elementos = elementos + " <li> " + arreglo[i] + "</br>";
+
+    }
+    document.getElementById(seccion).innerHTML = elementos;
+}
+
+
+function cambiarVisibilidad(seccion, tituloSeccion, tituloSeccionValue) {
+
+    if (document.getElementById(seccion).style.visibility == "hidden") {
+        document.getElementById(seccion).style.visibility = "visible";
+        document.getElementById(seccion).style.display = "block";
+        document.getElementById(tituloSeccion).innerHTML = tituloSeccionValue + " &#x25B2";
+    } else {
+        document.getElementById(seccion).style.visibility = "hidden";
+        document.getElementById(seccion).style.display = "none";
+        document.getElementById(tituloSeccion).innerHTML = tituloSeccionValue + "  &#x25BC;";
+
+    }
+
+}
+
+
+
+
+document.getElementById("misEstudiosBoton").addEventListener('click', function() {
+    cambiarVisibilidad("misEstudiosP", "misEstudiosBoton", "Formación académica");
+
+});
+
+document.getElementById("misConocimientosBoton").addEventListener('click', function() {
+    cambiarVisibilidad("misConocimientosP", "misConocimientosBoton", "Formación Laboral");
+
+});
+
+document.getElementById("datosInteresBoton").addEventListener('click', function() {
+    cambiarVisibilidad("datosInteresP", "datosInteresBoton", "Otros datos de interés");
+
+});
+
+
+
+
+const response = await fetch('http://localhost/curso-frontend/public/misDatos.json');
+const miCurriculum = await response.json();
+
+
+document.getElementById("nombre").innerHTML = miCurriculum.cv.nombre;
+document.getElementById("fecha").innerHTML = miCurriculum.cv.fechaNacimiento;
+document.getElementById("email").innerHTML = miCurriculum.cv.email;
+document.getElementById("telefono").innerHTML = miCurriculum.cv.telefono;
+
+
+var misEstudios = miCurriculum.cv.estudios;
+imprimirDatosComplejos("misEstudiosP", misEstudios);
+
+var conocimientos = miCurriculum.cv.laboral;
+imprimirDatosComplejos("misConocimientosP", conocimientos);
+
+var datosInteres = miCurriculum.cv.datosInteres;
+imprimirDatosComplejos("datosInteresP", datosInteres);
+
+
+document.getElementById("misEstudiosP").style.visibility = "hidden";
+document.getElementById("misEstudiosP").style.display = "none";
+
+document.getElementById("misConocimientosP").style.visibility = "hidden";
+document.getElementById("misConocimientosP").style.display = "none";
+
+document.getElementById("datosInteresP").style.visibility = "hidden";
+document.getElementById("datosInteresP").style.display = "none";
+
+document.getElementById("misEstudiosBoton").innerHTML = "Formación académica" + "  &#x25BC;";
+document.getElementById("misConocimientosBoton").innerHTML = "Formación Laboral" + "  &#x25BC;";
+document.getElementById("datosInteresBoton").innerHTML = "Otros datos de interés" + "  &#x25BC;";
